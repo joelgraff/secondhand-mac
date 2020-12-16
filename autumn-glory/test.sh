87d99601415e54e7d72e105ddf65112533fbcd31
Joel Graff <joel@thinkopenllc.co>
2020-12-16 15:39:19 -0600

    favorite-apps fix

--- autumn-glory/test.sh
+++ autumn-glory/test.sh
@@ -1,5 +1,2 @@
-apps=("'firefox.desktop'" 'thunderbird.desktop' 'org.gnome.Nautilus.desktop' 'rhythmbox.desktop' 'libreoffice-writer.desktop' 'snap-store_ubuntu-software.desktop' 'yelp.desktop' 'Zoom.desktop')
+dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'rhythmbox.desktop', 'libreoffice-writer.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'Zoom.desktop']"
 
-echo ${apps[*]}
-
-echo dconf write /org/gnome/shell/favorite-apps '"['${apps[*]}']"'
