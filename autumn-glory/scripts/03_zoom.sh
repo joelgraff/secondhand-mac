fdb3bba34002681cad4e085ce0026d44df458a67
joelgraff <monograff76@gmail.com>
2020-12-16 15:41:01 -0600

    finish zoom script

--- autumn-glory/scripts/03_zoom.sh
+++ autumn-glory/scripts/03_zoom.sh
@@ -5,3 +5,4 @@ sudo apt-get -f -y install
 sudo dpkg -i zoom_amd64.deb
 rm zoom_amd64.deb
 
+dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'rhythmbox.desktop', 'libreoffice-writer.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'Zoom.desktop']"
--- autumn-glory/test.sh
+++
@@ -1,2 +0,0 @@
-dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'rhythmbox.desktop', 'libreoffice-writer.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'Zoom.desktop']"
-
