eb7e9195602f1cc36be6e5c5d8b644973fc18b15
joelgraff <monograff76@gmail.com>
2020-12-17 07:43:29 -0600

    update zoom script

--- autumn-glory/scripts/03_zoom.sh
+++ autumn-glory/scripts/03_zoom.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd ~/Downloads
+
+sudo apt install -y libglib2.0-0 libgstreamer-plugins-base0.10-0 libxcb-shape0 ibxcb-shm0 libxcb-xfixes0 libxcb-randr0 libxcb-image0 libfontconfig1 libgl1-mesa-glx libxi6 libsm6 libxrender1 libpulse0 libxcomposite1 libxslt1.1 libsqlite3-0 libxcb-keysyms1 libxcb-xtest0
+
 wget https://zoom.us/client/latest/zoom_amd64.deb
-sudo apt-get -f -y install
+
 sudo dpkg -i zoom_amd64.deb
 rm zoom_amd64.deb
 
