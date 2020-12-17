de5601aee7c86cb5ff2ec93493384d14fc7f5c2d
Joel Graff <Joel Graff>
2020-12-17 09:56:17 -0600

    fix zoom deps

--- autumn-glory/scripts/03_zoom.sh
+++ autumn-glory/scripts/03_zoom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/Downloads
 
-sudo apt install -y libglib2.0-0 libgstreamer-plugins-base1.0-0 libxcb-shape0 libxcb-shm0 libxcb-xfixes0 libxcb-randr0 libxcb-image0 libfontconfig1 libgl1-mesa-glx libxi6 libsm6 libxrender1 libpulse0 libxcomposite1 libxslt1.1 libsqlite3-0 libxcb-keysyms1 libxcb-xtest0
+sudo apt install -y libglib2.0-0 libgstreamer-plugins-base1.0-0 libxcb-shape0 libxcb-shm0 libxcb-xfixes0 libxcb-randr0 libxcb-image0 libfontconfig1 libgl1-mesa-glx libxi6 libsm6 libxrender1 libpulse0 libxcomposite1 libxslt1.1 libsqlite3-0 libxcb-keysyms1 libxcb-xtest0 libegl1-mesa libxcb-xinerama0
 
 sudo apt --fix-broken -y install
 
