f6d55d7291e9e8e6db22ba3aa4945bbbe77dfcda
joelgraff <monograff76@gmail.com>
2020-12-21 09:46:28 -0600

    bug fix

--- autumn-glory/setup.sh
+++ autumn-glory/setup.sh
@@ -5,7 +5,7 @@ IFS=" " read -r -a script_list <<< ${scripts[*]}
 
 sudo -A mkdir /opt/secondhand-mac
 sudo cp opt/secondhand-mac/* /opt/secondhand-mac
-sudo cp user/shapre/applications/* /usr/share/applications
+sudo cp usr/share/applications/* /usr/share/applications
 
 dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'rhythmbox.desktop', 'libreoffice-writer.desktop', 'snap-store_ubuntu-software.desktop', 'yelp.desktop', 'shm_utils.desktop']"
 
