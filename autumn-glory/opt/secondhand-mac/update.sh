0ab00edbe987cb89d94ec5c78d454b9325822162
joelgraff <monograff76@gmail.com>
2021-01-01 16:34:56 -0600

    rear update fix

--- autumn-glory/opt/secondhand-mac/update.sh
+++ autumn-glory/opt/secondhand-mac/update.sh
@@ -28,6 +28,15 @@ tmp=$(mktemp -d)
 git clone http://github.com/joelgraff/secondhand-mac $tmp
 
 source=$tmp'/'$version'/desktop/*'
-echo source
 
 $(sudo -A cp $source /opt/secondhand-mac)
+
+#update rear
+cd /opt/secondhand-mac/rear
+sudo git pull
+
+#copy config files over
+sudo cp etc/rear/local.conf /opt/secondhand-mac/rear/etc/rear/local.conf
+sudo cp usr/share/rear/conf/* /opt/secondhand-mac/rear/usr/share/rear/conf
+sudo cp usr/share/rear/lib/* /opt/secondhand-mac/rear/usr/share/rear/lib
+sudo cp usr/share/rear/check/default/* /opt/secondhand-mac/rear/usr/share/rear/check/default
